0b073f16db6140140e41f85e7c5dc10af7ea50fe
Yorisoft <YelsinSepulveda@gmail.com>
2024-12-07 07:36:31 -0500

    Task 2 was much easier.. Hope it last
    I had the wrong idea at the start - was thinking about doing the same setup as task 1 but quickly realized it wont work to identify X patterns. Once I figure that out the solution came relatively quickly. For some reason it didnt work when I was using the if check for 'A' inside the xSearch function. Could have been that or the other checks I removed for bound checking.

--- Day_4/main.cpp
+++ Day_4/main.cpp
@@ -5,58 +5,49 @@
 
 class Solution {
 private: 	
-    std::string pattern = "XMAS";
-	std::vector<std::string> directions = {
-		"up/left", "up", "up/right",
-		"left", "right",
-		"down/left", "down", "down/right"
+    std::string xmass_pattern = "XMAS";
+	std::vector<std::pair<int, int>> xmass_directions = {
+		{-1, 0},	// up
+		{1, 0},		// down
+		{0, -1},	// left
+		{0, 1},		// right
+		{-1, -1},	// up-left
+		{-1, 1},	// up-right
+		{1, -1},	// down-left
+		{1, 1},		// down-right
 	};
 
-	double dfs(std::vector<std::vector<char>>* input, int r, int c, int index, std::string direction) {
-		if (index == pattern.size()) // index is equal to last index of pattern, meaning we've gotten to the end without missmatch.
-			return 1;
+	double circleSearch(std::vector<std::vector<char>>* input, int r, int c, int index, int dr, int dc) {
 		if (r >= (*input).size() || c >= (*input)[0].size() || r < 0 || c < 0) // if true, we're out of bounds 
 			return 0;
-		if ((*input)[r][c] != pattern[index]) // the current index in input does not match the current index in pattern, missmatch
+		if ((*input)[r][c] != xmass_pattern[index]) // the current index in input does not match the current index in pattern, missmatch
 			return 0;
+		if (index == xmass_pattern.size() - 1) // index is equal to pattern.size() - 1, meaning we've gotten to the end without missmatch.
+			return 1;
 
-		double result = 0;
-		if ((*input)[r][c] == pattern[index]){
-			if (direction == "up") {
-				// dfs up
-				result += dfs(input, r - 1, c, index + 1, "up");
-			}
-			// dfs up/right
-			if (direction == "up/right") {
-				result += dfs(input, r - 1, c + 1, index + 1, "up/right");
-			}
-			// dfs right
-			if (direction == "right") {
-				result += dfs(input, r, c + 1, index + 1, "right");
-			}
-			// dfs right/down
-			if (direction == "down/right") {
-				result += dfs(input, r + 1, c + 1, index + 1, "down/right");
-			}
-			// dfs down
-			if (direction == "down") {
-				result += dfs(input, r + 1, c, index + 1, "down");
-			}
-			// dfs down/left
-			if (direction == "down/left") {
-				result += dfs(input, r + 1, c - 1, index + 1, "down/left");
-			}
-			// dfs left
-			if (direction == "left") {
-				result += dfs(input, r, c - 1, index + 1, "left");
-			}
-			// dfs left/up
-			if (direction == "up/left") {
-				result += dfs(input, r - 1, c - 1, index + 1, "up/left");
-			}
-		}	
+		return circleSearch(input, r + dr, c + dc, index + 1, dr, dc);
+	}
+
+	double xSearch(std::vector<std::vector<char>>* input, int r, int c) {
+		if (r + 1 >= (*input).size() || c + 1 >= (*input)[0].size() || r - 1 < 0 || c - 1 < 0) // if true, we cannot create an X pattern from 'A' 
+			return 0;
 		
-		return result;
+		// we pass the checks for out of bounds and we found an 'A'
+		// Check if the A is surrounded by 'S' and 'M' in an X pattern and in a way that spells MAS
+		char up_left = (*input)[r - 1][c - 1];
+		char up_right = (*input)[r - 1][c + 1];
+		char down_left = (*input)[r + 1][c - 1];
+		char down_right = (*input)[r + 1][c + 1];
+		//first check if its sourrounded by S or M on the sides that matter, X pattern
+		// Check for MAS patterns in diagonals
+
+		// this doesnt work??? 
+		if ((up_left == 'M' && down_right == 'S' || up_left == 'S' && down_right == 'M') &&
+			(up_right == 'M' && down_left == 'S' || up_right == 'S' && down_left == 'M')) {
+			return 1;
+		} 
+
+		return 0;
 	}
 
 public:
@@ -65,12 +56,24 @@ public:
 
 	double taskOne(std::vector<std::vector<char>>* input) {
 		double answer = 0;
-		std::set<std::pair<int, int>> visted_index; 
 		for (int r = 0; r < input->size(); r++) {
 			for (int c = 0; c < (*input)[0].size(); c++) {
 				// search in all directions for each element. BUT maintain the direction per search.
-				for (int i = 0; i < directions.size(); i++) {
-					answer += dfs(input, r, c, 0, directions[i]);
+				for (std::pair<int, int> direction : xmass_directions) {
+					answer += circleSearch(input, r, c, 0, direction.first, direction.second);
+				}
+			}
+		}
+		return answer;
+	}
+
+	double taskTwo(std::vector<std::vector<char>>* input) {
+		double answer = 0;
+		for (int r = 0; r < input->size(); r++) {
+			for (int c = 0; c < (*input)[0].size(); c++) {
+				// search in all directions for each element. BUT maintain the direction per search.
+				if((*input)[r][c] == 'A'){
+					answer += xSearch(input, r, c);
 				}
 			}
 		}
@@ -92,5 +95,11 @@ int main(int argc, char* argv[]) {
 	std::cout << std::fixed << std::setprecision(0);
 	std::cout << "We calculate the answer to Day 4, Task 1 to be: " << answer << std::endl;
 	std::cin.get();
+
+	answer = solution->taskTwo(input);
+	std::cout << std::fixed << std::setprecision(0);
+	std::cout << "We calculate the answer to Day 4, Task 2 to be: " << answer << std::endl;
+	std::cin.get();
+
 }
 
