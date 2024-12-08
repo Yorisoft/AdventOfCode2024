bd672ab2baf7784f332efb347fe6802bc89bba64
Yorisoft <YelsinSepulveda@gmail.com>
2024-12-08 04:32:56 -0500

    Solution for day 5 task 1
    Answer:5991, response: Correct. Very happy that I was able to solve this on my own for the most part. Need to get more practice with iterators and pos's

--- Day_5/main.cc
+++ Day_5/main.cc
@@ -1,4 +1,5 @@
 #include "load_input_utils.cc"
+#include<iomanip>
 
 class Solution {
 private:
@@ -6,12 +7,33 @@ private:
 public:
     Solution() {};
     ~Solution() {};
+
+    double taskOne(std::vector<std::pair<int, int>>* input_1, std::vector<std::vector<int>>* input_2) {
+        double result = 0;
+        for (auto& pages : *input_2) {
+            bool isValid = true;
+            for (auto& rule : *input_1) {
+                std::vector<int>::iterator ruleOne = std::find(pages.begin(), pages.end(), rule.first);
+                std::vector<int>::iterator ruleTwo = std::find(pages.begin(), pages.end(), rule.second);
+                
+                if ((ruleOne != pages.end() && ruleTwo != pages.end()) && !(ruleOne < ruleTwo)) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                result += pages[pages.size()/2];  
+            } 
+        }
+        return result;
+    }
 };
 
 int main(int argc, char* argv[]) {
     std::vector<std::pair<int, int>>* input_1;
     std::vector<std::vector<int>>* input_2;
     Input* data = new Input();
+    Solution* solution = new Solution();
 
     std::string
         fileName_1 = "input_1.txt",
@@ -20,9 +42,14 @@ int main(int argc, char* argv[]) {
     input_1 = data->getInput_1(fileName_1);
     input_2 = data->getInput_2(fileName_2);
 
-
     data->printInput(input_1);
     data->printInput(input_2);
+    
+    int answer = solution->taskOne(input_1, input_2);
+
+    std::cout << std::fixed << std::setprecision(0);
+    std::cout << "We calculate the answer to Day 5, Task 1 to be: " << answer << std::endl;
+    std::cin.get();
 
     return 0;
 }
